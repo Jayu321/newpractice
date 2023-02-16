b8fb9652cffb91c52299a8fed2a8f8b78b330a03
DELL <DELL@DESKTOP-FOIF60P>
2023-02-16 16:01:43 +0000

    dxjcdkfcky

---
+++ string/src/stringmethods/samiksha.java
@@ -0,0 +1,19 @@
+package stringmethods;
+
+public class samiksha {public static void main(String[] args) {
+	
+	String s= "jayesh and nikhil ";
+	String reverse="";
+	for(int i= s.length()-1; i>=0; i--)
+	{
+		
+		reverse = reverse+s.charAt(i);
+	}
+	System.out.println(reverse);
+	
+	
+	System.out.println("Nikhil");
+	System.out.println("samiksha");
+}
+
+}
